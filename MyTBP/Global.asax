147ac75e9cbfb7c01937d40319725562be4b0c93
Timothy Edgar <tedgar@gmail.com>
2014-10-03 14:31:38 -0700

    Initial Project

---
+++ MyTBP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyTBP.MvcApplication" Language="C#" %>
---
+++ MyTBP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTBP.Startup))]
+namespace MyTBP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
